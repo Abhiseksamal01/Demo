1c76f454d632bd6855a955af64b68e7362f1cb12
Abhisek samal <samalabhisek2000@gmail.com>
2023-09-05 23:33:14 +0530

    master

--- Demo_SpringREST_DELETE/src/main/java/com/infy/entity/Customer.java
+++ Demo_SpringREST_DELETE/src/main/java/com/infy/entity/Customer.java
@@ -2,10 +2,10 @@ package com.infy.entity;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import com.infy.dto.CustomerDTO;
 
